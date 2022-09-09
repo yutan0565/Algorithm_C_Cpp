52452d480f859d7682536550513a58f051babb6c
KimYutan <yutan056575@gmail.com>
2022-09-09 17:05:41 +0900

    Create 25304.c

---
+++ Baekjoon_SBS/03/25304.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int x, n, a, b;
+    scanf("%d\n%d\n", &x, &n);
+
+    int total = 0;
+    int i;
+    for(i = 0 ; i < n ; i++){
+        scanf("%d %d", &a, &b);
+        total += (a*b);
+    }
+    if(total == x){
+        printf("%s", "Yes");
+    }else{
+        printf("%s", "No");
+    }
+
+}
