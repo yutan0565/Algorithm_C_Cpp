527a2003314c709ea843a31da6a8d82d09b99f4d
KimYutan <yutan056575@gmail.com>
2022-09-16 22:47:41 +0900

    Create 8958.c

---
+++ Baekjoon_SBS/04/8958.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    int n, i, j,len;
+    char s[80];
+    scanf("%d", &n);
+
+    int count, sum;
+    for(i=0; i < n ; i++){
+        scanf("%s",&s);
+        printf("%s\n",s);
+        len = strlen(s);
+        count = 0;
+        sum = 0;
+        for(j = 0 ; j < len ; j++){
+            if(s[j] =='O'){
+                count++;
+                sum += count;
+            }else{
+                count = 0;
+            }
+        }
+        printf("%d\n",sum);
+        
+    }
+
+
+
+}
