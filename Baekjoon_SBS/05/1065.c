c9e9d2197af5314774e9da9b5ca35d25fc8cfc1e
KimYutan <yutan056575@gmail.com>
2022-09-20 19:12:01 +0900

    Create 1065.c

---
+++ Baekjoon_SBS/05/1065.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int check(int n){
+    if(n < 100){
+        printf("%d", n);
+    }else{
+        int i, count = 99, a,b,c;
+        for(i=100;i<=n;i++){
+            a = i/100;
+            b = i/10;
+            c = i%10;
+            if((a-b) == (b-c)){
+                count++;
+            }
+        }
+        printf("%d", count);
+
+    }
+
+}
+
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    check(n);
+}
