39cc98b6cb9d500301f5b3f528b5fa893fd425c8
KimYutan <yutan056575@gmail.com>
2022-09-29 06:43:43 +0900

    Update 1316.c

--- Baekjoon_SBS/06/1316.c
+++ Baekjoon_SBS/06/1316.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    int n, count = 0, len;
-    char input[101];
-    scanf("%d", &n);
-
-    for(int i=0; i<n;i++){
-        scanf("%s", input);
-        len = strlen(input);
-        int flag = 1;
-        for(int j = 0 ; j< len ;j++){
-            char temp = input[j];
-            for(int k = j+2 ; k < len ; k++){
-                if(temp == input[k]) flag = 0;
-            }
-        }
-        if (flag) count++;
-    }
-    printf("%d", count);
+int main()
+{
+	
+	char input[101]; //문자열 배열
+	int n ; //input 값 N
+	scanf("%d", &n);
+	
+	int count = n; 
+	for (int i = 0; i < n; i++){
+		char first = '0';
+		int count_list[26] = {0, };
+		scanf("%s", input);
+		for (int j = 0; j < strlen(input); j++){
+			if (first != input[j]){
+				first = input[j];
+				count_list[input[j]-'a'] += 1;
+			}
+			if (count_list[input[j] - 'a'] == 2){
+				count -= 1;
+				break;
+			}
+		}
+	}
+	printf("%d", count);
+	return 0;
 }
