898a8972023dfd283be48ef9f9d70c65c373c398
KimYutan <yutan056575@gmail.com>
2022-10-10 11:04:47 +0900

    Create 1929.c

---
+++ Baekjoon_SBS/08/1929.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int isprime(int number){
+    if(number == 1) return 0;
+    for(int i = 2 ; i*i <= number ; i++){
+        if(number%i == 0) return 0;
+    }
+    return 1;
+}
+
+
+
+int main(){
+    int m, n ;
+    scanf("%d %d", &m, &n);
+    for(int i = m ; i <= n ; i++){
+        if(isprime(i)) printf("%d\n", i);
+    }
+    return 0;
+}
