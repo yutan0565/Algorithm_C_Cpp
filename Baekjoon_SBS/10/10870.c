0883de7e59799c5b7703c5bd3157b6cb0fbd55b3
KimYutan <yutan056575@gmail.com>
2022-10-18 23:12:39 +0900

    Create 10870.c

---
+++ Baekjoon_SBS/10/10870.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fibo(int n) {
+    if (n >= 2) return fibo(n-1) + fibo(n - 2);
+    else if (n == 1) return 1;
+    else return 0;
+}
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fibo(n));
+}
