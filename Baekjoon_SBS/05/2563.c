582b61ab51f113382a59ca0416ab1aa30821690d
KimYutan <yutan056575@gmail.com>
2022-10-29 10:14:18 +0900

    Create 2563.c

---
+++ Baekjoon_SBS/05/2563.c
@@ -0,0 +1,26 @@
+#include <stdio.h>  
+
+int main() {
+    int paper[100][100] = { 0, }; 
+    int n, w, h;
+    int cnt = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {  
+        scanf("%d %d", &w, &h);
+        for (int j = 100 - (h + 10); j < 100 - h; j++) {  
+            for (int k = w; k < w + 10; k++) {
+                paper[j][k] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 100; i++) { 
+        for (int j = 0; j < 100; j++) {  
+            if (paper[i][j] == 1) cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+    return 0;
+}
