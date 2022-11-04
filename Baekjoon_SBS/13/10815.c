6cae697a5eaacfe74785ed7e657ac4067a9d5a63
KimYutan <yutan056575@gmail.com>
2022-11-04 18:16:23 +0900

    Create 10815.c

---
+++ Baekjoon_SBS/13/10815.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, m;
+    scanf("%d", &n);
+    int n_list[n];
+    for(int i = 0 ; i < n; i++){
+        
+    }
+    scanf("%d", &m);
+
+    return 0;
+}
