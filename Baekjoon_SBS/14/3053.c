f72ac8bf8cd0c6330a200f794ed7c24c4852a2fe
KimYutan <yutan056575@gmail.com>
2022-11-13 23:40:15 +0900

    Update 3053.c

--- Baekjoon_SBS/14/3053.c
+++ Baekjoon_SBS/14/3053.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <math.h>
-#define _USE_MATH_DEFINES
+#define PI 3.14159265359
 
 int main(void){
     int r;
     double r_1, r_2;
     
     scanf("%d", &r);
-    r_1 = (double)r * r * M_PI;    
+    r_1 = (double)r * r * PI;    
     r_2 = (double)r * r * 2;  
     
     printf("%.6lf\n%.6lf", r_1, r_2);
