c009e805fb18119967a6253b142142c765ddeb1d
KimYutan <yutan056575@gmail.com>
2023-05-31 21:20:51 +0900

    Create 24265.cc

---
+++ Cpp/BaekJoon_SBS/11/24265.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ll n; scanf("%ld", &n);
+    printf("%ld\n%ld\n", n*(n-1)/2,2);
+    return 0;
+}
