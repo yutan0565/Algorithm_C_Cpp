2bdcdbf55718aa1bcefafb5b29204a58817cdbf1
KimYutan <yutan056575@gmail.com>
2023-06-01 22:43:58 +0900

    Create 24266.cc

---
+++ Cpp/BaekJoon_SBS/11/24266.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ll n; scanf("%ld", &n);
+    printf("%ld\n%ld\n", n*n*n,3);
+    return 0;
+}
